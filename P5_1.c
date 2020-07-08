3d0489fdeb9cbc5f002c00d577113c2273b316c4
Malay Bhavsar <55908198+Leo-Malay@users.noreply.github.com>
2020-07-08 12:03:25 +0530

    Add files via upload

---
+++ P5_1.c
@@ -0,0 +1,28 @@
+/*
+*   This program adds n number using malloc() and free()
+*   @Author: Malay Bhavsar
+*/
+
+#include <stdio.h>
+#include <cstdlib>
+
+int main()
+{
+    int *ptr, n, num, total = 0;
+    // Getting users input for number of number
+    printf("Enter number of numbers to be added: ");
+    scanf("%d", &n);
+    // Getting the memory block.
+    ptr = (int *)malloc(n * sizeof(int));
+    // Taking numbers as input and adding them.
+    for (int i = 0; i < n; i++)
+    {
+        printf("Number[%d]: ", i);
+        scanf("%d", ptr + i);
+        total += *(ptr + i);
+    }
+    printf("\nYour total is %d", total);
+    // De-allocating the memory we allocated
+    free(ptr);
+    return 0;
+}
